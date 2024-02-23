0deff7acccf4f401a6b2404fd4f6a656a4b2582f
Chris <chrisdietrich3666@gmail.com>
2024-02-22 20:22:57 -0500

    refactor useProjects for aws

--- hooks/useProjects.tsx
+++ hooks/useProjects.tsx
@@ -1,13 +1,43 @@
 import { useEffect, useState } from "react";
 import { Project } from "../model/project";
+import { DynamoDBResponse } from "../model/project";
+
+interface ApiGatewayResponse {
+  status: string;
+  body: string;
+}
 
 const useProjects = () => {
   const [projects, setProjects] = useState<Project[]>([]);
 
+  const convertToCard = (data: DynamoDBResponse[]): Project[] => {
+    return data.map((item) => ({
+      _id: { $oid: item.ID.S },
+      title: item.title.S,
+      image: item.image.S,
+      technologies: item.technologies.SS,
+      github_link: item.github_link.S,
+      link: item.link.S,
+      libraries: item.libraries.SS,
+      backend: item.backend.SS,
+    }));
+  };
+
   const fetchProjects = async () => {
-    const response = await fetch("/api/projects");
-    const projects = (await response.json()) as Project[];
-    return projects;
+    const response = await fetch(
+      "https://16glg8r950.execute-api.us-east-1.amazonaws.com/production/projects",
+      {
+        headers: {
+          "x-api-key": process.env.API_KEY,
+        },
+      }
+    );
+
+    console.log(response);
+
+    const projectsResponse = (await response.json()) as ApiGatewayResponse;
+    const projectsArray = convertToCard(JSON.parse(projectsResponse.body));
+    return projectsArray;
   };
 
   useEffect(() => {
@@ -22,4 +52,3 @@ const useProjects = () => {
 };
 
 export default useProjects;
-
--- model/project.ts
+++ model/project.ts
@@ -2,9 +2,19 @@ export interface Project {
   _id: { $oid: string };
   title: string;
   image: string;
-  github_link: string;
   technologies: string[];
+  github_link: string;
   link: string;
   libraries: string[];
   backend: string[];
+}
+export interface DynamoDBResponse {
+  ID: { S: string };
+  title: { S: string };
+  image: { S: string };
+  technologies: { SS: string[] };
+  github_link: { S: string };
+  link: { S: string };
+  libraries: { SS: string[] };
+  backend: { SS: string[] };
 }
